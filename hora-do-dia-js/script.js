da37c1659fb7247a403ef2f0def65575b01b7aeb
leonardo santos <sant0s.le022zera@gmail.com>
2026-01-04 22:03:51 -0300

    Add files via upload

---
+++ hora-do-dia-js/script.js
@@ -0,0 +1,19 @@
+function carregar() {
+ var msg = window.document.getElementById('msg')
+var img = window.document.getElementById("imagem")
+var data = new Date()
+//var hora = data.getHours()
+var hora = 9
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora <12){
+    // BOM DIA!
+    img.src = "manha.jpg"
+} else if (hora >= 12 && hora < 18) {
+    //BOA TARDE!
+    img.src = "tarde.jpg"
+} else {
+    //BOA NOITE!
+    img.src = "noite.jpg"
+
+}
+}
